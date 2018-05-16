0be2252c71c287505dfeebd8ec4aeaf339fba218
Greatest Dane <DaneGoodman85@gmail.com>
2018-05-15 20:05:45 -0700

    Got the counter to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,18 @@
+var gameOver = false;
 var count=120;
 var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
 
 
 //Create the countdown function
  function timer() {
-  counter;
   count --;
-  if (count <= 0)
+  if (count < 0)
   {
      clearInterval(counter);
      //counter ended, do something here
-     return;
+     return gameOver = true;
   }
-
+  $("#time-remaining").html(count);
 };
 
-window.onload = counter;
 // Dispaly the countdown information
-$("#time-remaining").html(count);
