058452f660795abc8f333e54e182d9b7157b429a
Greatest Dane <DaneGoodman85@gmail.com>
2018-05-17 19:19:38 -0700

    I think I'm done with this

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -43,6 +43,92 @@ function gameEnd() {
   $("#time-left").text(count);
 }
 
+// Game logic function
+
+function gameLogic() {
+  answer1 = $('.question1:checked').val();
+  if (answer1 === answers.answer1) {
+    answersCorrect ++;
+  }
+  else {
+    answersWrong ++;
+  }
+  //question 2
+  answer2 = $('.question2:checked').val();
+  if (answer2 === answers.answer2) {
+    answersCorrect ++;
+  }
+  else {
+    answersWrong ++;
+  }
+  //question 3
+  answer3 = $('.question3:checked').val();
+  if (answer3 === answers.answer3) {
+    answersCorrect ++;
+  }
+  else {
+    answersWrong ++;
+  }
+  //question 4
+  answer4 = $('.question4:checked').val();
+  if (answer4 === answers.answer4) {
+    answersCorrect ++;
+  }
+  else {
+    answersWrong ++;
+  }
+  //question 5
+  answer5 = $('.question5:checked').val();
+  if (answer5 === answers.answer5) {
+    answersCorrect ++;
+  }
+  else {
+    answersWrong ++;
+  }
+   //question 6
+   answer6 = $('.question6:checked').val();
+   if (answer6 === answers.answer6) {
+     answersCorrect ++;
+   }
+   else {
+     answersWrong ++;
+   }
+   //question 7
+   answer7 = $('.question7:checked').val();
+   if (answer7 === answers.answer7) {
+     answersCorrect ++;
+   }
+   else {
+     answersWrong ++;
+   }
+   //question 8
+   answer8 = $('.question8:checked').val();
+   if (answer8 === answers.answer8) {
+     answersCorrect ++;
+   }
+   else {
+     answersWrong ++;
+   }
+   //question 9
+   answer9 = $('.question9:checked').val();
+   if (answer9 === answers.answer9) {
+     answersCorrect ++;
+   }
+   else {
+     answersWrong ++;
+   }
+     //question 10
+     answer10 = $('.question10:checked').val();
+     if (answer10 === answers.answer10) {
+       answersCorrect ++;
+     }
+     else {
+       answersWrong ++;
+     }
+
+}
+
+
 // Make it so when you press the start button the game begins and the counter begins
 $("#start-button").on("click", function(){
 
@@ -64,130 +150,22 @@ $("#start-button").on("click", function(){
     gameOver = true;
   }
   if (gameOver === true) {
+    gameLogic();
     gameEnd();
   };
 };
 
+
 // if the submit button is pressed, the game is over
 $("#submit-button").on("click", function() {
+ 
+  gameLogic();
+
   gameOver = true;
   if (gameOver === true) {
     gameEnd();
     gameOver = false;
   };
-});
-
-
-
-// Game logic and onclick functions
-// question 1
-$(".question1").on("click", function() {
-  answer1 = $(this).attr("value");
-  if (answer1 === answers.answer1) {
-    answersCorrect ++;
-  }
-  else {
-    answersWrong ++;
-  }
-});
-
-//question 2
-$(".question2").on("click", function() {
-  answer2 = $(this).attr("value");
-  if (answer2 === answers.answer2) {
-    answersCorrect ++;
-  }
-  else {
-    answersWrong ++;
-  }
-});
-
-// question 3
-$(".question3").on("click", function() {
-  answer3 = $(this).attr("value");
-  if (answer3 === answers.answer3) {
-    answersCorrect ++;
-  }
-  else {
-    answersWrong ++;
-  }
-});
-
-// question 4
-$(".question4").on("click", function() {
-  answer4 = $(this).attr("value");
-  if (answer4 === answers.answer4) {
-    answersCorrect ++;
-  }
-  else {
-    answersWrong ++;
-  }
-});
-
-// question 5
-$(".question5").on("click", function() {
-  answer5 = $(this).attr("value");
-  if (answer5 === answers.answer5) {
-    answersCorrect ++;
-  }
-  else {
-    answersWrong ++;
-  }
-});
-
-// question 6
-$(".question6").on("click", function() {
-  answer6 = $(this).attr("value");
-  if (answer6 === answers.answer6) {
-    answersCorrect ++;
-  }
-  else {
-    answersWrong ++;
-  }
-});
-
-// question 7
-$(".question7").on("click", function() {
-  answer7 = $(this).attr("value");
-  if (answer7 === answers.answer7) {
-    answersCorrect ++;
-  }
-  else {
-    answersWrong ++;
-  }
-});
-
-// question 8
-$(".question8").on("click", function() {
-  answer8 = $(this).attr("value");
-  if (answer8 === answers.answer8) {
-    answersCorrect ++;
-  }
-  else {
-    answersWrong ++;
-  }
-});
-
-// question 9
-$(".question9").on("click", function() {
-  answer9 = $(this).attr("value");
-  if (answer9 === answers.answer9) {
-    answersCorrect ++;
-  }
-  else {
-    answersWrong ++;
-  }
-});
-
-// question 10
-$(".question10").on("click", function() {
-  answer10 = $(this).attr("value");
-  if (answer10 === answers.answer10) {
-    answersCorrect ++;
-  }
-  else {
-    answersWrong ++;
-  }
 });
 
 });
